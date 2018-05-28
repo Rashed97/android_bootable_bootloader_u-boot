OE4T/u-boot-tegra

ARM: tegra: implement DT copying/deleting when booting kernel
In some configurations, U-Boot is chain-loaded by another bootloader, and
that bootloader passes a DTB to the next stage. Generally, that DTB is
intended for consumption by an NVIDIA L4T kernel, and hence uses non-
upstreamed DT bindings, and hence is not suitable for consumption by
U-Boot as a control DTB. However, the other bootloader has often placed
useful data into that DTB, such as SDRAM training data or L4T-specific
board enumeration data, and this data must be passed on to any L4T kernel
that is eventually booted.

This patch implements a feature that allows copying a set of DT nodes and
properties from that DTB into the DTB that's passed to the kernel. The
list of nodes/properties to copy is taken from environment variables,
which allows the user to configure or disable this operation. Future
patches will configure the default environment to contain the required
copying configuration on a per-SoC basis.

Equally, there are times when certain nodes/properties must be deleted
from the DTB that's passed to the kernel. This patch also implementes
that, again configured via environment variables.

Bug 200211165

Change-Id: Ibea20f204b42dc9ecad6d02a2600b82b88bb0b8a
Signed-off-by: Stephen Warren <swarren@nvidia.com>
Reviewed-on: http://git-master/r/1195409
Reviewed-by: Tom Warren <twarren@nvidia.com>

arch/arm/mach-tegra/dt-edit.c
@@ -1,0 +1,349 @@
+/*
+ *  Copyright (C) 2010-2016 NVIDIA CORPORATION.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <stdlib.h>
+#include <common.h>
+#include <fdt_support.h>
+#include <fdtdec.h>
+#include "dt-edit.h"
+
+#define fdt_for_each_property(fdt, prop, parent)		\
+	for (prop = fdt_first_property_offset(fdt, parent);	\
+	     prop >= 0;						\
+	     prop = fdt_next_property_offset(fdt, prop))
+
+typedef int iter_envitem(void *blob_dst, char *item, void *param);
+
+static int fdt_copy_node_content(void *blob_src, int ofs_src, void *blob_dst,
+		int ofs_dst, int indent)
+{
+	int ofs_src_child, ofs_dst_child;
+
+	/*
+	 * FIXME: This doesn't remove properties or nodes in the destination
+	 * that are not present in the source. For the nodes we care about
+	 * right now, this is not an issue.
+	 */
+
+	fdt_for_each_property(blob_src, ofs_src_child, ofs_src) {
+		const void *prop;
+		const char *name;
+		int len, ret;
+
+		prop = fdt_getprop_by_offset(blob_src, ofs_src_child, &name,
+					     &len);
+		debug("%s: %*scopy prop: %s\n", __func__, indent, "", name);
+
+		ret = fdt_setprop(blob_dst, ofs_dst, name, prop, len);
+		if (ret < 0) {
+			error("Can't copy DT prop %s\n", name);
+			return ret;
+		}
+	}
+
+	fdt_for_each_subnode(blob_src, ofs_src_child, ofs_src) {
+		const char *name;
+
+		name = fdt_get_name(blob_src, ofs_src_child, NULL);
+		debug("%s: %*scopy node: %s\n", __func__, indent, "", name);
+
+		ofs_dst_child = fdt_subnode_offset(blob_dst, ofs_dst, name);
+		if (ofs_dst_child < 0) {
+			debug("%s: %*s(creating it in dst)\n", __func__,
+			      indent, "");
+			ofs_dst_child = fdt_add_subnode(blob_dst, ofs_dst,
+							name);
+			if (ofs_dst_child < 0) {
+				error("Can't copy DT node %s\n", name);
+				return ofs_dst_child;
+			}
+		}
+
+		fdt_copy_node_content(blob_src, ofs_src_child, blob_dst,
+				      ofs_dst_child, indent + 2);
+	}
+
+	return 0;
+}
+
+static int fdt_add_path(void *blob, const char *path)
+{
+	char *pcopy, *tmp, *node;
+	int ofs_parent, ofs_child, ret;
+
+	if (path[0] != '/') {
+		error("Can't add path %s; missing leading /", path);
+		return -1;
+	}
+	path++;
+	if (!*path) {
+		debug("%s: path points at DT root!", __func__);
+		return 0;
+	}
+
+	pcopy = strdup(path);
+	if (!pcopy) {
+		error("strdup() failed");
+		return -1;
+	}
+
+	tmp = pcopy;
+	ofs_parent = 0;
+	while (true) {
+		node = strsep(&tmp, "/");
+		if (!node)
+			break;
+		debug("%s: node=%s\n", __func__, node);
+		ofs_child = fdt_subnode_offset(blob, ofs_parent, node);
+		if (ofs_child < 0)
+			ofs_child = fdt_add_subnode(blob, ofs_parent, node);
+		if (ofs_child < 0) {
+			error("Can't create DT node %s\n", node);
+			ret = ofs_child;
+			goto out;
+		}
+		ofs_parent = ofs_child;
+	}
+	ret = ofs_parent;
+
+out:
+	free(pcopy);
+
+	return ret;
+}
+
+static iter_envitem fdt_iter_copy_prop;
+static int fdt_iter_copy_prop(void *blob_dst, char *prop_path, void *blob_src)
+{
+	char *prop_name, *node_path;
+	const void *prop;
+	int ofs_src, ofs_dst, len, ret;
+
+	prop_name = strrchr(prop_path, '/');
+	if (!prop_name) {
+		error("Can't copy prop %s; missing /", prop_path);
+		return -1;
+	}
+	*prop_name = 0;
+	prop_name++;
+	node_path = prop_path;
+
+	if (*node_path) {
+		ofs_src = fdt_path_offset(blob_src, node_path);
+		if (ofs_src < 0) {
+			error("DT node %s missing in source; can't copy %s\n",
+			      node_path, prop_name);
+			return -1;
+		}
+
+		ofs_dst = fdt_path_offset(blob_dst, node_path);
+		if (ofs_src < 0) {
+			error("DT node %s missing in dest; can't copy prop %s\n",
+			      node_path, prop_name);
+			return -1;
+		}
+	} else {
+		ofs_src = 0;
+		ofs_dst = 0;
+	}
+
+	prop = fdt_getprop(blob_src, ofs_src, prop_name, &len);
+	if (!prop) {
+		error("DT property %s/%s missing in source; can't copy\n",
+		      node_path, prop_name);
+		return -1;
+	}
+
+	ret = fdt_setprop(blob_dst, ofs_dst, prop_name, prop, len);
+	if (ret < 0) {
+		error("Can't set DT prop %s/%s\n", node_path, prop_name);
+		return ret;
+	}
+
+	return 0;
+}
+
+static iter_envitem fdt_iter_copy_node;
+static int fdt_iter_copy_node(void *blob_dst, char *path, void *blob_src)
+{
+	int ofs_dst, ofs_src;
+	int ret;
+
+	ofs_dst = fdt_add_path(blob_dst, path);
+	if (ofs_dst < 0) {
+		error("Can't find/create dest DT node %s to copy\n", path);
+		return ofs_dst;
+	}
+
+	if (!fdtdec_get_is_enabled(blob_dst, ofs_dst)) {
+		debug("%s: DT node %s disabled in dest; skipping copy\n",
+			__func__, path);
+		return 0;
+	}
+
+	ofs_src = fdt_path_offset(blob_src, path);
+	if (ofs_src < 0) {
+		error("DT node %s missing in source; can't copy\n", path);
+		return 0;
+	}
+
+	ret = fdt_copy_node_content(blob_src, ofs_src, blob_dst,
+				    ofs_dst, 2);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static iter_envitem fdt_iter_del_node;
+static int fdt_iter_del_node(void *blob_dst, char *node_path, void *unused_param)
+{
+	int ofs;
+
+	ofs = fdt_path_offset(blob_dst, node_path);
+	/* Node doesn't exist -> property can't exist -> it's removed! */
+	if (ofs == -FDT_ERR_NOTFOUND)
+		return 0;
+	if (ofs < 0) {
+		error("DT node %s lookup failure; can't del node\n", node_path);
+		return ofs;
+	}
+
+	return fdt_del_node(blob_dst, ofs);
+}
+
+static iter_envitem fdt_iter_del_prop;
+static int fdt_iter_del_prop(void *blob_dst, char *prop_path, void *unused_param)
+{
+	char *prop_name, *node_path;
+	int ofs, ret;
+
+	prop_name = strrchr(prop_path, '/');
+	if (!prop_name) {
+		error("Can't del prop %s; missing /", prop_path);
+		return -1;
+	}
+	*prop_name = 0;
+	prop_name++;
+	node_path = prop_path;
+
+	if (*node_path) {
+		ofs = fdt_path_offset(blob_dst, node_path);
+		/* Node doesn't exist -> property can't exist -> it's removed! */
+		if (ofs == -FDT_ERR_NOTFOUND)
+			return 0;
+		if (ofs < 0) {
+			error("DT node %s lookup failure; can't del prop %s\n",
+			      node_path, prop_name);
+			return ofs;
+		}
+	} else {
+		ofs = 0;
+	}
+
+	ret = fdt_delprop(blob_dst, ofs, prop_name);
+	/* Property doesn't exist -> it's already removed! */
+	if (ret == -FDT_ERR_NOTFOUND)
+		return 0;
+	return ret;
+}
+
+static int fdt_iter_envlist(iter_envitem *func, void *blob_dst, const char *env_varname, void *param)
+{
+	char *items, *tmp, *item;
+	int ret;
+
+	items = getenv(env_varname);
+	if (!items) {
+		debug("%s: No env var %s\n", __func__, env_varname);
+		return 0;
+	}
+
+	items = strdup(items);
+	if (!items) {
+		error("strdup(%s) failed", env_varname);
+		return -1;
+	}
+
+	tmp = items;
+	while (true) {
+		item = strsep(&tmp, ":");
+		if (!item)
+			break;
+		debug("%s: item: %s\n", __func__, item);
+		ret = func(blob_dst, item, param);
+		if (ret < 0) {
+			ret = -1;
+			goto out;
+		}
+	}
+
+	ret = 0;
+
+out:
+	free(items);
+
+	return ret;
+}
+
+__weak void *fdt_copy_get_blob_src_default(void)
+{
+	return NULL;
+}
+
+static void *fdt_get_copy_blob_src(void)
+{
+	char *src_addr_s;
+
+	src_addr_s = getenv("fdt_copy_src_addr");
+	if (!src_addr_s)
+		return fdt_copy_get_blob_src_default();
+	return (void *)simple_strtoul(src_addr_s, NULL, 16);
+}
+
+int fdt_copy_env_nodelist(void *blob_dst)
+{
+	void *blob_src;
+
+	debug("%s:\n", __func__);
+
+	blob_src = fdt_get_copy_blob_src();
+	if (!blob_src) {
+		debug("%s: No source DT\n", __func__);
+		return 0;
+	}
+
+	return fdt_iter_envlist(fdt_iter_copy_node, blob_dst, "fdt_copy_node_paths", blob_src);
+}
+
+int fdt_copy_env_proplist(void *blob_dst)
+{
+	void *blob_src;
+
+	debug("%s:\n", __func__);
+
+	blob_src = fdt_get_copy_blob_src();
+	if (!blob_src) {
+		debug("%s: No source DT\n", __func__);
+		return 0;
+	}
+
+	return fdt_iter_envlist(fdt_iter_copy_prop, blob_dst, "fdt_copy_prop_paths", blob_src);
+}
+
+int fdt_del_env_nodelist(void *blob_dst)
+{
+	debug("%s:\n", __func__);
+
+	return fdt_iter_envlist(fdt_iter_del_node, blob_dst, "fdt_del_node_paths", NULL);
+}
+
+int fdt_del_env_proplist(void *blob_dst)
+{
+	debug("%s:\n", __func__);
+
+	return fdt_iter_envlist(fdt_iter_del_prop, blob_dst, "fdt_del_prop_paths", NULL);
+}

Rashed97/android_bootable_bootloader_u-boot

arch/arm/mach-tegra/dt-edit.c
@@ -1,0 +1,349 @@
+/*
+ *  Copyright (C) 2010-2016 NVIDIA CORPORATION.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <stdlib.h>
+#include <common.h>
+#include <fdt_support.h>
+#include <fdtdec.h>
+#include "dt-edit.h"
+
+#define fdt_for_each_property(fdt, prop, parent)		\
+	for (prop = fdt_first_property_offset(fdt, parent);	\
+	     prop >= 0;						\
+	     prop = fdt_next_property_offset(fdt, prop))
+
+typedef int iter_envitem(void *blob_dst, char *item, void *param);
+
+static int fdt_copy_node_content(void *blob_src, int ofs_src, void *blob_dst,
+		int ofs_dst, int indent)
+{
+	int ofs_src_child, ofs_dst_child;
+
+	/*
+	 * FIXME: This doesn't remove properties or nodes in the destination
+	 * that are not present in the source. For the nodes we care about
+	 * right now, this is not an issue.
+	 */
+
+	fdt_for_each_property(blob_src, ofs_src_child, ofs_src) {
+		const void *prop;
+		const char *name;
+		int len, ret;
+
+		prop = fdt_getprop_by_offset(blob_src, ofs_src_child, &name,
+					     &len);
+		debug("%s: %*scopy prop: %s\n", __func__, indent, "", name);
+
+		ret = fdt_setprop(blob_dst, ofs_dst, name, prop, len);
+		if (ret < 0) {
+			pr_err("Can't copy DT prop %s\n", name);
+			return ret;
+		}
+	}
+
+	fdt_for_each_subnode(blob_src, ofs_src_child, ofs_src) {
+		const char *name;
+
+		name = fdt_get_name(blob_src, ofs_src_child, NULL);
+		debug("%s: %*scopy node: %s\n", __func__, indent, "", name);
+
+		ofs_dst_child = fdt_subnode_offset(blob_dst, ofs_dst, name);
+		if (ofs_dst_child < 0) {
+			debug("%s: %*s(creating it in dst)\n", __func__,
+			      indent, "");
+			ofs_dst_child = fdt_add_subnode(blob_dst, ofs_dst,
+							name);
+			if (ofs_dst_child < 0) {
+				pr_err("Can't copy DT node %s\n", name);
+				return ofs_dst_child;
+			}
+		}
+
+		fdt_copy_node_content(blob_src, ofs_src_child, blob_dst,
+				      ofs_dst_child, indent + 2);
+	}
+
+	return 0;
+}
+
+static int fdt_add_path(void *blob, const char *path)
+{
+	char *pcopy, *tmp, *node;
+	int ofs_parent, ofs_child, ret;
+
+	if (path[0] != '/') {
+		pr_err("Can't add path %s; missing leading /", path);
+		return -1;
+	}
+	path++;
+	if (!*path) {
+		debug("%s: path points at DT root!", __func__);
+		return 0;
+	}
+
+	pcopy = strdup(path);
+	if (!pcopy) {
+		pr_err("strdup() failed");
+		return -1;
+	}
+
+	tmp = pcopy;
+	ofs_parent = 0;
+	while (true) {
+		node = strsep(&tmp, "/");
+		if (!node)
+			break;
+		debug("%s: node=%s\n", __func__, node);
+		ofs_child = fdt_subnode_offset(blob, ofs_parent, node);
+		if (ofs_child < 0)
+			ofs_child = fdt_add_subnode(blob, ofs_parent, node);
+		if (ofs_child < 0) {
+			pr_err("Can't create DT node %s\n", node);
+			ret = ofs_child;
+			goto out;
+		}
+		ofs_parent = ofs_child;
+	}
+	ret = ofs_parent;
+
+out:
+	free(pcopy);
+
+	return ret;
+}
+
+static iter_envitem fdt_iter_copy_prop;
+static int fdt_iter_copy_prop(void *blob_dst, char *prop_path, void *blob_src)
+{
+	char *prop_name, *node_path;
+	const void *prop;
+	int ofs_src, ofs_dst, len, ret;
+
+	prop_name = strrchr(prop_path, '/');
+	if (!prop_name) {
+		pr_err("Can't copy prop %s; missing /", prop_path);
+		return -1;
+	}
+	*prop_name = 0;
+	prop_name++;
+	node_path = prop_path;
+
+	if (*node_path) {
+		ofs_src = fdt_path_offset(blob_src, node_path);
+		if (ofs_src < 0) {
+			pr_err("DT node %s missing in source; can't copy %s\n",
+			      node_path, prop_name);
+			return -1;
+		}
+
+		ofs_dst = fdt_path_offset(blob_dst, node_path);
+		if (ofs_src < 0) {
+			pr_err("DT node %s missing in dest; can't copy prop %s\n",
+			      node_path, prop_name);
+			return -1;
+		}
+	} else {
+		ofs_src = 0;
+		ofs_dst = 0;
+	}
+
+	prop = fdt_getprop(blob_src, ofs_src, prop_name, &len);
+	if (!prop) {
+		pr_err("DT property %s/%s missing in source; can't copy\n",
+		      node_path, prop_name);
+		return -1;
+	}
+
+	ret = fdt_setprop(blob_dst, ofs_dst, prop_name, prop, len);
+	if (ret < 0) {
+		pr_err("Can't set DT prop %s/%s\n", node_path, prop_name);
+		return ret;
+	}
+
+	return 0;
+}
+
+static iter_envitem fdt_iter_copy_node;
+static int fdt_iter_copy_node(void *blob_dst, char *path, void *blob_src)
+{
+	int ofs_dst, ofs_src;
+	int ret;
+
+	ofs_dst = fdt_add_path(blob_dst, path);
+	if (ofs_dst < 0) {
+		pr_err("Can't find/create dest DT node %s to copy\n", path);
+		return ofs_dst;
+	}
+
+	if (!fdtdec_get_is_enabled(blob_dst, ofs_dst)) {
+		debug("%s: DT node %s disabled in dest; skipping copy\n",
+			__func__, path);
+		return 0;
+	}
+
+	ofs_src = fdt_path_offset(blob_src, path);
+	if (ofs_src < 0) {
+		pr_err("DT node %s missing in source; can't copy\n", path);
+		return 0;
+	}
+
+	ret = fdt_copy_node_content(blob_src, ofs_src, blob_dst,
+				    ofs_dst, 2);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static iter_envitem fdt_iter_del_node;
+static int fdt_iter_del_node(void *blob_dst, char *node_path, void *unused_param)
+{
+	int ofs;
+
+	ofs = fdt_path_offset(blob_dst, node_path);
+	/* Node doesn't exist -> property can't exist -> it's removed! */
+	if (ofs == -FDT_ERR_NOTFOUND)
+		return 0;
+	if (ofs < 0) {
+		pr_err("DT node %s lookup failure; can't del node\n", node_path);
+		return ofs;
+	}
+
+	return fdt_del_node(blob_dst, ofs);
+}
+
+static iter_envitem fdt_iter_del_prop;
+static int fdt_iter_del_prop(void *blob_dst, char *prop_path, void *unused_param)
+{
+	char *prop_name, *node_path;
+	int ofs, ret;
+
+	prop_name = strrchr(prop_path, '/');
+	if (!prop_name) {
+		pr_err("Can't del prop %s; missing /", prop_path);
+		return -1;
+	}
+	*prop_name = 0;
+	prop_name++;
+	node_path = prop_path;
+
+	if (*node_path) {
+		ofs = fdt_path_offset(blob_dst, node_path);
+		/* Node doesn't exist -> property can't exist -> it's removed! */
+		if (ofs == -FDT_ERR_NOTFOUND)
+			return 0;
+		if (ofs < 0) {
+			pr_err("DT node %s lookup failure; can't del prop %s\n",
+			      node_path, prop_name);
+			return ofs;
+		}
+	} else {
+		ofs = 0;
+	}
+
+	ret = fdt_delprop(blob_dst, ofs, prop_name);
+	/* Property doesn't exist -> it's already removed! */
+	if (ret == -FDT_ERR_NOTFOUND)
+		return 0;
+	return ret;
+}
+
+static int fdt_iter_envlist(iter_envitem *func, void *blob_dst, const char *env_varname, void *param)
+{
+	char *items, *tmp, *item;
+	int ret;
+
+	items = env_get(env_varname);
+	if (!items) {
+		debug("%s: No env var %s\n", __func__, env_varname);
+		return 0;
+	}
+
+	items = strdup(items);
+	if (!items) {
+		pr_err("strdup(%s) failed", env_varname);
+		return -1;
+	}
+
+	tmp = items;
+	while (true) {
+		item = strsep(&tmp, ":");
+		if (!item)
+			break;
+		debug("%s: item: %s\n", __func__, item);
+		ret = func(blob_dst, item, param);
+		if (ret < 0) {
+			ret = -1;
+			goto out;
+		}
+	}
+
+	ret = 0;
+
+out:
+	free(items);
+
+	return ret;
+}
+
+__weak void *fdt_copy_get_blob_src_default(void)
+{
+	return NULL;
+}
+
+static void *fdt_get_copy_blob_src(void)
+{
+	char *src_addr_s;
+
+	src_addr_s = env_get("fdt_copy_src_addr");
+	if (!src_addr_s)
+		return fdt_copy_get_blob_src_default();
+	return (void *)simple_strtoul(src_addr_s, NULL, 16);
+}
+
+int fdt_copy_env_nodelist(void *blob_dst)
+{
+	void *blob_src;
+
+	debug("%s:\n", __func__);
+
+	blob_src = fdt_get_copy_blob_src();
+	if (!blob_src) {
+		debug("%s: No source DT\n", __func__);
+		return 0;
+	}
+
+	return fdt_iter_envlist(fdt_iter_copy_node, blob_dst, "fdt_copy_node_paths", blob_src);
+}
+
+int fdt_copy_env_proplist(void *blob_dst)
+{
+	void *blob_src;
+
+	debug("%s:\n", __func__);
+
+	blob_src = fdt_get_copy_blob_src();
+	if (!blob_src) {
+		debug("%s: No source DT\n", __func__);
+		return 0;
+	}
+
+	return fdt_iter_envlist(fdt_iter_copy_prop, blob_dst, "fdt_copy_prop_paths", blob_src);
+}
+
+int fdt_del_env_nodelist(void *blob_dst)
+{
+	debug("%s:\n", __func__);
+
+	return fdt_iter_envlist(fdt_iter_del_node, blob_dst, "fdt_del_node_paths", NULL);
+}
+
+int fdt_del_env_proplist(void *blob_dst)
+{
+	debug("%s:\n", __func__);
+
+	return fdt_iter_envlist(fdt_iter_del_prop, blob_dst, "fdt_del_prop_paths", NULL);
+}