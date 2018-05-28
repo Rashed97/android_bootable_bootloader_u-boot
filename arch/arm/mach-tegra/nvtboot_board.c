OE4T/u-boot-tegra

T210: use T186 'nvtboot' code in mach-tegra, w/changes for CBoot
Uses x0 pointer for DTB, get /memory node for DRAM size, a la
T186 U-Boot. Also uses DTB 'bootargs' to find and set env
variables 'lp0_vec' and 'nvdumper_reserved'.

Boots OK on my Jetson-TX1 as a faux kernel via CBoot, and
if configured to use nvtboot_cpu (prior to these changes).

Use CONFIG_CPU_BL_IS_CBOOT=1 to enable this new boot path.
Otherwise it's the same as it was before (using nvtboot_cpu).

Change-Id: I2548532502fbe410b1ba32ca3924297d69dd5751
Signed-off-by: Tom Warren <twarren@nvidia.com>
Reviewed-on: http://git-master/r/1284317
Reviewed-by: Stephen Warren <swarren@nvidia.com>
GVS: Gerrit_Virtual_Submit

arch/arm/mach-tegra/nvtboot_board.c
@@ -1,9 +1,10 @@
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.
+ * Copyright (c) 2016-2017, NVIDIA CORPORATION.
  *
  * SPDX-License-Identifier: GPL-2.0+
  */
 
+#include <stdlib.h>
 #include <common.h>
 #include <fdt_support.h>
 #include <fdtdec.h>
@@ -24,6 +25,7 @@ static int set_fdt_addr(void)
 	return 0;
 }
 
+#if defined(CONFIG_TEGRA186)
 /*
  * Attempt to use /chosen/nvidia,ether-mac in the nvtboot DTB to U-Boot's
  * ethaddr environment variable if possible.
@@ -57,16 +59,102 @@ static int set_ethaddr_from_nvtboot(void)
 
 	return 0;
 }
+#endif	/* T186 */
 
-int tegra_soc_board_init_late(void)
+#if defined(CONFIG_TEGRA210)
+static int parse_bootargs(void)
+{
+	const void *nvtboot_blob = (void *)nvtboot_boot_x0;
+	const void *prop;
+	const char *start;
+	char *bargs, *s;
+	char *target;
+	int node, len;
+
+	/*
+	 * Set some env vars based on the bootargs passed in
+	 * the DTB by the previous bootloader (pointer in reg x0)
+	 */
+
+	debug("%s: nvtboot_blob = %p\n", __func__, nvtboot_blob);
+
+	node = fdt_path_offset(nvtboot_blob, "/chosen");
+	if (node < 0) {
+		error("Can't find /chosen node in nvtboot DTB");
+		return node;
+	}
+	debug("%s: found 'chosen' node: %d\n", __func__, node);
+
+	prop = fdt_getprop(nvtboot_blob, node, "bootargs", &len);
+	if (!prop) {
+		error("Can't find /chosen/bootargs property in nvtboot DTB");
+		return -ENOENT;
+	}
+	debug("%s: found 'bootargs' property, len =%d\n",  __func__, len);
+
+	bargs = strdup((char *)prop);
+	debug("%s: bootargs = %s\n", __func__, bargs);
+
+	/* Get nvdumper_reserved from bootargs */
+	target = "nvdumper_reserved";
+	start = bargs;
+
+	s = strstr(start, target);
+	if (s) {
+		debug("%s: Found %s in bootargs.\n", __func__, target);
+
+		/* remove 'nvdumper_reserved=' portion */
+		start = strtok(s, "=");
+		/* get actual hex address portion */
+		start = strtok(NULL, " ");
+		debug("%s: %s=%s\n", __func__, target, start);
+		setenv("nvdumper_reserved", start);
+	} else {				/* s == NULL */
+		debug("%s: Failed to find %s in bootargs!\n", __func__, target);
+	}
+
+	/* Re-allocate bargs since strtok above has modified it */
+	free(bargs);
+	bargs = strdup((char *)prop);
+
+	/* Get lp0_vec from bootargs */
+	target = "lp0_vec";
+	start = bargs;
+
+	s = strstr(start, target);
+	if (s) {
+		debug("%s: Found %s in bootargs.\n", __func__, target);
+
+		/* remove 'lp0_vec=' portion */
+		start = strtok(s, "=");
+		/* get actual hex address portion */
+		start = strtok(NULL, " ");
+		debug("%s: %s=%s\n", __func__, target, start);
+		setenv("lp0_vec", start);
+	} else {				/* s == NULL */
+		debug("%s: Failed to find %s in bootargs!\n", __func__, target);
+	}
+
+	free(bargs);
+	return 0;
+}
+#endif	/* T210 */
+
+int nvtboot_init_late(void)
 {
 	/*
 	 * Ignore errors here; the value may not be used depending on
 	 * extlinux.conf or boot script content.
 	 */
 	set_fdt_addr();
+#if defined(CONFIG_TEGRA186)
 	/* Ignore errors here; not all cases care about Ethernet addresses */
 	set_ethaddr_from_nvtboot();
+#endif
+#if defined(CONFIG_TEGRA210)
+	/* Handle lp0_vec and nvdumper env vars */
+	parse_bootargs();
+#endif
 
 	return 0;
 }

Rashed97/android_bootable_bootloader_u-boot

arch/arm/mach-tegra/nvtboot_board.c
@@ -283,6 +283,7 @@ static int set_fdt_addr(void)
 	return 0;
 }
 
+#if defined(CONFIG_TEGRA186)
 /*
  * Attempt to use /chosen/nvidia,ether-mac in the nvtboot DTB to U-Boot's
  * ethaddr environment variable if possible.
@@ -316,8 +317,88 @@ static int set_ethaddr_from_nvtboot(void)
 
 	return 0;
 }
+#endif	/* T186 */
 
-int tegra_soc_board_init_late(void)
+#if defined(CONFIG_TEGRA210)
+static int parse_bootargs(void)
+{
+	const void *nvtboot_blob = (void *)nvtboot_boot_x0;
+	const void *prop;
+	const char *start;
+	char *bargs, *s;
+	char *target;
+	int node, len;
+
+	/*
+	 * Set some env vars based on the bootargs passed in
+	 * the DTB by the previous bootloader (pointer in reg x0)
+	 */
+
+	debug("%s: nvtboot_blob = %p\n", __func__, nvtboot_blob);
+
+	node = fdt_path_offset(nvtboot_blob, "/chosen");
+	if (node < 0) {
+		pr_err("Can't find /chosen node in nvtboot DTB");
+		return node;
+	}
+	debug("%s: found 'chosen' node: %d\n", __func__, node);
+
+	prop = fdt_getprop(nvtboot_blob, node, "bootargs", &len);
+	if (!prop) {
+		pr_err("Can't find /chosen/bootargs property in nvtboot DTB");
+		return -ENOENT;
+	}
+	debug("%s: found 'bootargs' property, len =%d\n",  __func__, len);
+
+	bargs = strdup((char *)prop);
+	debug("%s: bootargs = %s\n", __func__, bargs);
+
+	/* Get nvdumper_reserved from bootargs */
+	target = "nvdumper_reserved";
+	start = bargs;
+
+	s = strstr(start, target);
+	if (s) {
+		debug("%s: Found %s in bootargs.\n", __func__, target);
+
+		/* remove 'nvdumper_reserved=' portion */
+		start = strtok(s, "=");
+		/* get actual hex address portion */
+		start = strtok(NULL, " ");
+		debug("%s: %s=%s\n", __func__, target, start);
+		setenv("nvdumper_reserved", start);
+	} else {				/* s == NULL */
+		debug("%s: Failed to find %s in bootargs!\n", __func__, target);
+	}
+
+	/* Re-allocate bargs since strtok above has modified it */
+	free(bargs);
+	bargs = strdup((char *)prop);
+
+	/* Get lp0_vec from bootargs */
+	target = "lp0_vec";
+	start = bargs;
+
+	s = strstr(start, target);
+	if (s) {
+		debug("%s: Found %s in bootargs.\n", __func__, target);
+
+		/* remove 'lp0_vec=' portion */
+		start = strtok(s, "=");
+		/* get actual hex address portion */
+		start = strtok(NULL, " ");
+		debug("%s: %s=%s\n", __func__, target, start);
+		setenv("lp0_vec", start);
+	} else {				/* s == NULL */
+		debug("%s: Failed to find %s in bootargs!\n", __func__, target);
+	}
+
+	free(bargs);
+	return 0;
+}
+#endif	/* T210 */
+
+int nvtboot_init_late(void)
 {
 	set_calculated_env_vars();
 	/*
@@ -325,8 +406,14 @@ int tegra_soc_board_init_late(void)
 	 * extlinux.conf or boot script content.
 	 */
 	set_fdt_addr();
+#if defined(CONFIG_TEGRA186)
 	/* Ignore errors here; not all cases care about Ethernet addresses */
 	set_ethaddr_from_nvtboot();
+#endif
+#if defined(CONFIG_TEGRA210)
+	/* Handle lp0_vec and nvdumper env vars */
+	parse_bootargs();
+#endif
 
 	return 0;
 }